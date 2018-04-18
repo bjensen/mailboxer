bf276ba37f1cde7713950c71f5ccdb1225d72687
Brian Jensen <briajens@gmail.com>
2018-04-18 23:23:45 +0200

    removed use of attr_accessible

--- app/models/mailboxer/receipt.rb
+++ app/models/mailboxer/receipt.rb
@@ -1,6 +1,5 @@
 class Mailboxer::Receipt < ActiveRecord::Base
   self.table_name = :mailboxer_receipts
-  attr_accessible :trashed, :is_read, :deleted if Mailboxer.protected_attributes?
 
   belongs_to :notification, :class_name => "Mailboxer::Notification"
   belongs_to :receiver, :polymorphic => :true, :required => false
